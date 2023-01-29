54dab0fc3a7150dbc2939b2760dac37a2b59d29e
spacefall <91277246+spacefall@users.noreply.github.com>
2023-01-29 23:27:53 +0100

    It downloads images now

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@
 
 mod net;
 
+use std::env::temp_dir;
+
+
 fn main() {
     net::check_network(5);
-    let json = net::get_json();
-    println!("Url: {:?}\nSource: {:?}", json.url, json.source);
+    net::get_image(temp_dir().join("walle.jpg").into_os_string().into_string().unwrap());
     //let args: Vec<String> = env::args().collect();
     //dbg!(&args);
     //let _ = futures::executor::block_on(apply_wallpaper_lockscreen(&args[1]));
--- src/net.rs
+++ src/net.rs
@@ -1,7 +1,8 @@
 use serde::Deserialize;
 use online::check;
-use std::process;
-use reqwest::blocking;
+use std::process::exit;
+use reqwest::*;
+use std::fs::File;
 
 #[derive(Deserialize)]
 pub struct Stalewall {
@@ -15,12 +16,19 @@ pub fn get_json() -> Stalewall {
     json
 }
 
+pub fn get_image(path: String) {
+   let api = blocking::get("https://stalewall.vercel.app/api").unwrap();
+   let json: Stalewall = api.json().unwrap();
+   let mut file = File::create(path).unwrap();
+   let _ = blocking::get(json.url).unwrap().copy_to(&mut file);
+}
+
 pub fn check_network(timeout: u64) {
     let network_state: bool = check(Some(timeout)).is_ok();
     if network_state {
         println!("Connected to the internet!");
     } else {
         println!("Couldn't connect to the internet after $time");
-        process::exit(1);
+        exit(1);
     }
 }

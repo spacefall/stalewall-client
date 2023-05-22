4bea82ece2e533b1e495b68605715a72b930ca44
spacefall <91277246+spacefall@users.noreply.github.com>
2023-05-22 16:07:16 +0200

    Made it do what it needs, except error handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,51 @@
 //use std::env;
 
-
 //#[cfg(windows)]
 //mod windows;
 
 mod net;
 
-use std::env::temp_dir;
-
+use futures::executor::block_on;
+use std::{env, time::Duration};
+use wallpaper;
+use windows::{
+    core::*,
+    Storage::{FileAccessMode, StorageFile},
+};
 
 fn main() {
-    net::check_network(5);
-    net::get_image(temp_dir().join("walle.jpg").into_os_string().into_string().unwrap());
-    //let args: Vec<String> = env::args().collect();
-    //dbg!(&args);
-    //let _ = futures::executor::block_on(apply_wallpaper_lockscreen(&args[1]));
+    // Consts
+    const TIMEOUT: Duration = Duration::new(5, 0);
+
+    // check network
+    println!(
+        "Checking for connection to the internet, timeout: {}s",
+        TIMEOUT.as_secs(),
+    );
+    net::check_network(TIMEOUT);
+
+    // setup tempdir path
+    let wallpath: String = env::temp_dir()
+        .join("stalewall.jpg")
+        .into_os_string()
+        .into_string()
+        .unwrap();
+    // print path
+    println!("Image path: {}", wallpath);
+    // download image
+    println!("Downloading image");
+    net::get_image(&wallpath);
+    // set desktop background
+    println!("Applying desktop wallpaper");
+    wallpaper::set_from_path(&wallpath).unwrap();
+    // set lockscreen wallpaper
+    block_on(set_lockscreen(wallpath)).unwrap();
+}
+
+async fn set_lockscreen(path: String) -> Result<()> {
+    println!("Applying lockscreen wallpaper");
+    let file = StorageFile::GetFileFromPathAsync(&HSTRING::from(path))?.await?;
+    let stream = file.OpenAsync(FileAccessMode::Read)?.await?;
+    windows::System::UserProfile::LockScreen::SetImageStreamAsync(&stream)?.await?;
+    Ok(())
 }
--- src/net.rs
+++ src/net.rs
@@ -1,8 +1,11 @@
-use serde::Deserialize;
-use online::check;
-use std::process::exit;
+//use online::check;
+use ping::ping;
 use reqwest::*;
+use serde::Deserialize;
 use std::fs::File;
+use std::net::IpAddr;
+use std::process::exit;
+use std::time::Duration;
 
 #[derive(Deserialize)]
 pub struct Stalewall {
@@ -10,25 +13,24 @@ pub struct Stalewall {
     pub url: String,
 }
 
-pub fn get_json() -> Stalewall {
-    let response = blocking::get("https://stalewall.vercel.app/api").unwrap();
-    let json: Stalewall = response.json().unwrap();
-    json
+pub fn get_image(path: &String) {
+    let api = blocking::get("https://stalewall.vercel.app/api").unwrap();
+    let json: Stalewall = api.json().unwrap();
+    let mut file = File::create(path).unwrap();
+    blocking::get(json.url).unwrap().copy_to(&mut file).unwrap();
 }
 
-pub fn get_image(path: String) {
-   let api = blocking::get("https://stalewall.vercel.app/api").unwrap();
-   let json: Stalewall = api.json().unwrap();
-   let mut file = File::create(path).unwrap();
-   let _ = blocking::get(json.url).unwrap().copy_to(&mut file);
-}
+pub fn check_network(timeout: Duration) {
+    // http://detectportal.firefox.com/success.txt
+    let pingaddr: IpAddr = "34.107.221.82".parse().unwrap();
+    let pinger = ping(pingaddr, Some(timeout), Some(112), None, None, None);
 
-pub fn check_network(timeout: u64) {
-    let network_state: bool = check(Some(timeout)).is_ok();
-    if network_state {
-        println!("Connected to the internet!");
-    } else {
-        println!("Couldn't connect to the internet after $time");
-        exit(1);
+    // Basic error handling, if it doesn't error it just prints connected otherwise print not connected, the error and exit with code 1
+    match pinger {
+        Ok(_) => println!("Connected to the internet"),
+        Err(e) => {
+            println!("Not connected to the internet, error follows:\n{}", e);
+            exit(1);
+        }
     }
 }
